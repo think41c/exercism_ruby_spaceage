699633db3907d60cac19182ab2ec4cd72d725ef5
Tony DiNitto <think41c@gmail.com>
2015-05-02 19:11:33 -0400

    Extraction of orbit values for different planets.

--- space_age.rb
+++ space_age.rb
@@ -1,16 +1,11 @@
-   # - Earth: orbital period 365.25 Earth days, or 31557600 seconds
-   # - Mercury: orbital period 0.2408467 Earth years
-   # - Venus: orbital period 0.61519726 Earth years
-   # - Mars: orbital period 1.8808158 Earth years
-   # - Jupiter: orbital period 11.862615 Earth years
-   # - Saturn: orbital period 29.447498 Earth years
-   # - Uranus: orbital period 84.016846 Earth years
-   # - Neptune: orbital period 164.79132 Earth years
-
 class SpaceAge
 
   def initialize(seconds)
     @seconds = seconds
+    yearly_orbit_in_days
+  end
+
+  def yearly_orbit_in_days
     @earth   = 365.25000000
     @venus   =   0.61519726
     @mars    =   1.88081580
@@ -27,9 +22,6 @@ class SpaceAge
   def on_earth
     @seconds / 60 / 60 / 24 / @earth
   end
-
-
-
 end
 
 a = SpaceAge.new(1000000000)
