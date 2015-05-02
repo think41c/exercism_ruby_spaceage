5ab3ddc3a3ea23c6556a3c8bed1c8b6c7bdc5413
Tony DiNitto <think41c@gmail.com>
2015-05-01 20:58:03 -0400

    Basic logic implemented for computing seconds.

---
+++ deeelete.rb
@@ -0,0 +1,7 @@
+a = 5
+b = 6
+puts a + b
+puts "Hello" 
+puts a
+puts b
+puts a + 5
--- space_age.rb
+++ space_age.rb
@@ -1,9 +1,28 @@
+   # - Earth: orbital period 365.25 Earth days, or 31557600 seconds
+   # - Mercury: orbital period 0.2408467 Earth years
+   # - Venus: orbital period 0.61519726 Earth years
+   # - Mars: orbital period 1.8808158 Earth years
+   # - Jupiter: orbital period 11.862615 Earth years
+   # - Saturn: orbital period 29.447498 Earth years
+   # - Uranus: orbital period 84.016846 Earth years
+   # - Neptune: orbital period 164.79132 Earth years
+
 class SpaceAge
-  def initialize(x)
+
+  def initialize(seconds)
+    @seconds = seconds
   end
 
 	def seconds
-    1_000_000
+    @seconds   
 	end
 	
+  def on_earth
+    seconds_in_a_year = 365.25 * 24 * 60 * 60
+    puts seconds_in_a_year*31
+  end
+
 end
+
+a = SpaceAge.new(2)
+a.on_earth
